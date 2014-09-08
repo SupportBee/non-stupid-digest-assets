abf643cbc1cb8f63ea94ec2e50ecf73037a9d9fa
Subin T P <subinthattaparambil@gmail.com>
2014-09-08 11:27:28 +0530

    Creates directory if necessery

--- lib/non-stupid-digest-assets.rb
+++ lib/non-stupid-digest-assets.rb
@@ -23,7 +23,7 @@ end
 module Sprockets
   class Manifest
     def compile_with_non_digest *args
-      compile_without_non_digest *args
+      compile_without_non_digest *args  
 
       NonStupidDigestAssets.files(files).each do |(digest_path, info)|
         full_digest_path = File.join dir, digest_path
@@ -33,19 +33,13 @@ module Sprockets
 
         if File.exists? full_digest_path
           logger.debug "Writing #{full_non_digest_path}"
+          FileUtils.mkdir_p(File.dirname(full_non_digest_path))
           FileUtils.copy_file full_digest_path, full_non_digest_path, :preserve_attributes
         else
           logger.debug "Could not find: #{full_digest_path}"
-        end
-        # if File.exists? full_digest_gz_path
-        #   logger.debug "Writing #{full_non_digest_gz_path}"
-        #   FileUtils.copy_file full_digest_gz_path, full_non_digest_gz_path, :preserve_attributes
-        # else
-        #   logger.debug "Could not find: #{full_digest_gz_path}"
-        # end
+        end    
       end
     end
-
     alias_method_chain :compile, :non_digest
   end
 end
